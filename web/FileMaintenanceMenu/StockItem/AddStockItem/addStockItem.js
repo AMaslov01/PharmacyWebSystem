51b01f0669626c597b7ced555b8729cc6cff7611
Evgenii Salnikov <ems2312227@gmail.com>
2024-02-13 23:18:57 +0000

    started php

--- web/FileMaintenanceMenu/StockItem/AddStockItem/addStockItem.js
+++ web/FileMaintenanceMenu/StockItem/AddStockItem/addStockItem.js
@@ -13,29 +13,10 @@ document.getElementById('menu_button').addEventListener('click', function() {
     }
 });
 
-
 function validateForm(){
-    // Get the values entered in the email and confirm email fields
-    var email = document.getElementById('email').value;
-    var confirmEmail = document.getElementById('confirm_email').value;
+    return confirmChanges();
+}
 
-    // Check if the entered emails match
-    if (email != confirmEmail) {
-        alert("Emails do not match!");
-        return false; // Prevent form submission if emails don't match
-    }
-
-    // Call validateAge function to check age restriction (at least 17 years old)
-    if (!validateAge()){
-        alert("You must be at least 17 years old!");
-        return false; // Prevent form submission if age condition is not met
-    }
-
-    // Call validateStartDate function to ensure start date is not before today
-    if (!validateStartDate()){
-        alert("Starting Date must not be before today's date!");
-        return false; // Prevent form submission if start date is invalid
-    }
-
-    return true; // Allow form submission if all validations pass
+function confirmChanges(){
+    return confirm("Are you sure you want to make these changes?");
 }
---
+++ web/FileMaintenanceMenu/StockItem/AddStockItem/addStockItem.php
@@ -0,0 +1,16 @@
+<?php
+    include '../../../db.inc.php'; // Including the database configuration file
+
+    // Constructing the SQL query to insert data into the 'student' table
+    $sql = "Insert into Stock (description, costPrice, retailPrice, reorderLevel, reorderQuantity, supplierStockCode, quantityInStock) VALUES ('$_POST[description]','$_POST[costPrice]','$_POST[retailPrice]','$_POST[reorderLevel]','$_POST[reorderQuantity]','$_POST[supplierStockCode]','$_POST[quantityInStock]')";
+
+    // Executing the SQL query
+    if(!mysqli_query($con, $sql))
+    {
+        die ("An Error in the SQL Query: " . mysqli_error($con) ); // Displaying error message if query execution fails
+    }
+
+    echo "<br>A record has been added for " . $_POST['stockID'] . "."; // Success message
+
+    mysqli_close($con); // Closing the database connection
+?>
