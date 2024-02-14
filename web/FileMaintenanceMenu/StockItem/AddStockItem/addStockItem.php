0543821cb2c7f351b5114ad79ff07c2f4570e6b4
Evgenii Salnikov <ems2312227@gmail.com>
2024-02-14 13:00:24 +0000

    updated db.inc.php

--- web/FileMaintenanceMenu/StockItem/AddStockItem/addStockItem.php
+++ web/FileMaintenanceMenu/StockItem/AddStockItem/addStockItem.php
@@ -1,16 +1,17 @@
 <?php
-    include '../../../db.inc.php'; // Including the database configuration file
+include '../../../db.inc.php'; // Including the database configuration file
 
-    // Constructing the SQL query to insert data into the 'student' table
-    $sql = "Insert into Stock (description, costPrice, retailPrice, reorderLevel, reorderQuantity, supplierStockCode, quantityInStock) VALUES ('$_POST[description]','$_POST[costPrice]','$_POST[retailPrice]','$_POST[reorderLevel]','$_POST[reorderQuantity]','$_POST[supplierStockCode]','$_POST[quantityInStock]')";
+// Constructing the SQL query to insert data into the 'student' table
+$sql = "Insert into Stock (description, costPrice, retailPrice, reorderLevel, reorderQuantity, supplierStockCode, quantityInStock) VALUES ('$_POST[description]','$_POST[costPrice]','$_POST[retailPrice]','$_POST[reorderLevel]','$_POST[reorderQuantity]','$_POST[supplierStockCode]','$_POST[quantityInStock]')";
 
-    // Executing the SQL query
+// Executing the SQL query
+if (!empty($con)) {
     if(!mysqli_query($con, $sql))
     {
         die ("An Error in the SQL Query: " . mysqli_error($con) ); // Displaying error message if query execution fails
     }
+}
 
-    echo "<br>A record has been added for " . $_POST['stockID'] . "."; // Success message
+echo "<br>A record has been added for " . $_POST['stockID'] . "."; // Success message
 
-    mysqli_close($con); // Closing the database connection
-?>
+mysqli_close($con); // Closing the database connection
--- web/db.inc.php
+++ web/db.inc.php
@@ -1,14 +1,10 @@
 <?php
 $hostname = "localhost:3306";
-$username = "healthfirst";
-$password = "bsw81@6M1";
+//  $username = "healthfirst";
+$username = "root"; // username for local db
+// $password = "bsw81@6M1";
+$password = ""; // password for local db
 
 $dbname = "PharmacyWebSystem";
 
-$con = mysqli_connect($hostname, $username, $password, $dbname);
-
-if (!$con)
-{
-	die ("Failed to connect to MySQL: ".mysqli_connect_error());
-}
-?>
+$con = mysqli_connect($hostname, $username, $password, $dbname) or die ("Failed to connect to MySQL: ".mysqli_connect_error());
